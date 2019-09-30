cc61ed90725c0851e9395d59a779e41b46231c28
Barbara Muesing <muesingb@gmail.com>
2019-09-30 22:25:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,11 @@ data.each do |attribute, attribute_subcat|
   attribute_subcat.each do |attribute_sub, name_array|
       name_array.each do |name_array|
         pidgeon_list[name_array] = {attribute => [attribute_sub]}
-        if attribute == :color
-        pigeon_list[name_array][attribute] << attribute_sub
-      else
-        pigeon_list[name_array][attribute] = attribute_sub.to_s
-      end
+      #  if attribute == :color
+      #  pigeon_list[name_array][attribute] << attribute_sub
+    #  else
+      #  pigeon_list[name_array][attribute] = attribute_sub.to_s
+    #  end
         end
       end
     end

e13de44b86466a1da441df67537156e2e927c325
Barbara Muesing <muesingb@gmail.com>
2019-09-30 22:26:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ def nyc_pigeon_organizer(data)
 pidgeon_list = {}
 data.each do |attribute, attribute_subcat|
   attribute_subcat.each do |attribute_sub, name_array|
-      name_array.each do |name_array, name|
-        pidgeon_list[name_array] = {}
+      name_array.each do |name_array|
+        pidgeon_list[name_array] = {attribute => [attribute_sub]}
         end
       end
     end
